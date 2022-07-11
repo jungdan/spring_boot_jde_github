209f124db3c046d7523d3b0ebb9499a587104b93
DESKTOP-0ABRT42\PC-06 <ju304da@hanmail.net>
2022-07-11 10:36:18 +0900

    controller추가

---
+++ src/main/java/edu/hi/controller/HelloController.java
@@ -0,0 +1,14 @@
+package edu.hi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController{
+
+	@GetMapping("/")
+	public String Home() {
+		return "HelloWorld";
+	}
+
+}
